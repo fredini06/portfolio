f4ff73a03e13f69c1646277891f44463f761bb6d
Frederic Iniesta <fredini06@gmail.com>
2021-05-19 18:22:06 +0200

    header

---
+++ src/components/Header.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Typed from 'react-typed';
+
+const Header = () => {
+    return (
+        <div className="header-wraper">
+            <div className="main-info">
+                <h1>Développement Web & Infographie</h1>
+                <Typed 
+                    className="typed-text"
+                    strings={["Web design", "Développement Web", "Frontend", "Backend", "Infographie", "Mise en page", "Design"]}
+                    typeSpeed={40}
+                    backSpeed={60}
+                    loop
+                />
+                <a href="#" className="btn-header">Contactez-moi</a>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
